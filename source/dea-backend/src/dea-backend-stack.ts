765e71ac40c2ff9bba73e5e79eec1e21f78dfdaa
chowchin <chowchin@amazon.com>
2022-09-20 14:33:37 -0500

    hello world api
    cr: https://code.amazon.com/reviews/CR-76313553

--- source/dea-backend/src/dea-backend-stack.ts
+++ source/dea-backend/src/dea-backend-stack.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+import * as path from "path";
 import { LoggingService } from "@aws/workbench-core-logging";
 import * as cdk from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -7,11 +11,16 @@ export class DeaBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "src")),
+      handler: "hello.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdktestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    // eslint-disable-next-line no-new
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
