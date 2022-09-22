bc5f8391c2fb6f3b7bf2bd37606373790506fcbf
Jennifer Clark <clrknje@amazon.com>
2022-09-22 13:17:29 -0700

    Adds DEA UI
    SIM: https://i.amazon.com/V716766190
    cr: https://code.amazon.com/reviews/CR-76456255

---
+++ source/dea-ui/src/pages/index.tsx
@@ -0,0 +1,57 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import Box from '@awsui/components-react/box';
+import Link from '@awsui/components-react/link';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Hero from '../components/Hero';
+import { useNotifications } from '../context/NotificationContext';
+import { useSettings } from '../context/SettingsContext';
+
+export interface HomeProps {
+  locale: string;
+}
+
+
+const Home: NextPage = () => {
+  const { settings } = useSettings();
+  const { displayNotification } = useNotifications();
+
+
+  displayNotification('temp', {
+    type: 'info',
+    header: 'This is a sample notification',
+    content: (
+      <span>
+        We can use HTML here for the notifaction body
+        <Link href="#/" color="inverted">
+          link
+        </Link>
+        , or{' '}
+        <Link external href="#/" color="inverted">
+          another link
+        </Link>
+        .
+      </span>
+    )
+  });
+
+  return (
+    <Box margin={{ bottom: 'l' }}>
+      <Head>
+        <title>{settings.name}</title>
+        <meta name="description" content={settings.description} />
+        <link rel="icon" href={settings.favicon} />
+      </Head>
+
+      <Hero />
+
+      
+    </Box>
+  );
+};
+
+export default Home;
