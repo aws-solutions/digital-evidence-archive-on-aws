ff709dfc21792ee074ee3e4498c5b004b5befcfb
Jennifer Clark <clrknje@amazon.com>
2022-09-22 16:35:46 -0700

    Updates comment

--- source/dea-ui/infrastructure/src/constants.ts
+++ source/dea-ui/infrastructure/src/constants.ts
@@ -33,7 +33,6 @@ function getConstants(): {
 
   // CloudFormation Output Keys
   const S3_ARTIFACT_BUCKET_ARN_OUTPUT_KEY = 'S3BucketArtifactsArnOutput';
-  // The output name below must match the value in swb-reference
   const S3_ACCESS_LOGS_BUCKET_NAME_OUTPUT_KEY = 'S3BucketAccessLogsNameOutput';
 
   return {
