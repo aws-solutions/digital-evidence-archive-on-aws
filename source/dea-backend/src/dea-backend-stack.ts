4b605fae5d3e58af47dfed3fe7fec68714e5055a
chowchin <chowchin@amazon.com>
2022-09-23 06:07:09 +0000

    DEA-App init package

--- source/dea-backend/src/dea-backend-stack.ts
+++ source/dea-backend/src/dea-backend-stack.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import * as path from "path";
-import { LoggingService } from "@aws/workbench-core-logging";
 import * as cdk from "aws-cdk-lib";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DeaBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
