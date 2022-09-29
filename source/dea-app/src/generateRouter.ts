efcddb1d1f9c853a121505cdae2ccb4035603a5b
chowchin <chowchin@amazon.com>
2022-09-29 05:37:52 +0000

    commit: bd6ad29c2afec4b9ceab9fb1e3cc1b41573bcaf3 Author: chowchin <chowchin@amazon.com> Date: 2022-09-29T05:36:38.000Z
    feat(dea backend): express router, es build bundling, backend apigw & lambda

---
+++ source/dea-app/src/generateRouter.ts
@@ -0,0 +1,44 @@
+import cors from "cors";
+import express = require("express");
+import { Router, Express, Request, Response } from "express";
+import { ApiRoute, ApiRouteConfig } from "./apiRouteConfig";
+import { setUpDSRoutes } from "./datasetRoutes";
+import { boomErrorHandler, unknownErrorHandler } from "./errorHandlers";
+
+export function generateRouter(apiRouteConfig: ApiRouteConfig): Express {
+  const app: Express = express();
+  const router: Router = express.Router();
+
+  app.use(cors({ origin: apiRouteConfig.allowedOrigins }));
+  // parse application/json
+  app.use(express.json());
+
+  // Dynamic routes
+  apiRouteConfig.routes.forEach((apiRoute: ApiRoute) => {
+    // Config setting is provided by developer, and not external user request
+    // nosemgrep
+    router[apiRoute.httpMethod](
+      apiRoute.path,
+      async (req: Request, res: Response) => {
+        // Config setting is provided by developer, and not external user request
+        // nosemgrep
+        const response = await apiRoute.service[apiRoute.serviceAction]();
+        res.send(response);
+      }
+    );
+  });
+
+  setUpDSRoutes(
+    router,
+    apiRouteConfig.dataSetService,
+    apiRouteConfig.dataSetsStoragePlugin
+  );
+
+  // Error handling. Order of the error handlers is important
+  router.use(boomErrorHandler);
+  router.use(unknownErrorHandler);
+
+  app.use("/", router);
+
+  return app;
+}
--- source/dea-backend/src/dea-backend-stack.ts
+++ source/dea-backend/src/dea-backend-stack.ts
@@ -1,24 +1,86 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable no-new */
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
-import * as path from "path";
-import * as cdk from "aws-cdk-lib";
-import * as apigw from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs";
+import { join } from 'path';
+import * as path from 'path';
+import { Stack, CfnOutput, StackProps, Duration } from 'aws-cdk-lib';
+import {
+  AccessLogFormat,
+  LambdaIntegration,
+  LogGroupLogDestination,
+  RestApi
+} from 'aws-cdk-lib/aws-apigateway';
+import { Alias, Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as nodejsLambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
 
-export class DeaBackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class DeaBackendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, "HelloHandler", {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset(path.join(__dirname, "..", "src")),
-      handler: "hello.handler",
+    const apiLambda: NodejsFunction = this._createAPILambda();
+    this._createRestApi(apiLambda);
+  }
+
+  // Create Lambda
+  private _createAPILambda(): NodejsFunction {
+    const lambdaService = new nodejsLambda.NodejsFunction(this, 'dea-app-handler', {
+      memorySize: 512,
+      timeout: Duration.minutes(3),
+      runtime: Runtime.NODEJS_16_X,
+      handler: 'handler',
+      entry: path.join(__dirname, '/../src/backendAPILambda.ts'),
+      depsLockFilePath: join(__dirname, '/../../common/config/rush/pnpm-lock.yaml'),
+      bundling: {
+        externalModules: ['aws-sdk']
+      }
     });
 
-    // defines an API Gateway REST API resource backed by our "hello" function.
-    // eslint-disable-next-line no-new
-    new apigw.LambdaRestApi(this, "Endpoint", {
-      handler: hello,
+    return lambdaService;
+  }
+
+  // API Gateway
+  private _createRestApi(apiLambda: NodejsFunction): void {
+    const logGroup = new LogGroup(this, 'APIGatewayAccessLogs');
+    const API: RestApi = new RestApi(this, `API-Gateway API`, {
+      restApiName: 'Backend API Name',
+      description: 'Backend API',
+      deployOptions: {
+        stageName: 'dev',
+        accessLogDestination: new LogGroupLogDestination(logGroup),
+        accessLogFormat: AccessLogFormat.custom(
+          JSON.stringify({
+            stage: '$context.stage',
+            requestId: '$context.requestId',
+            integrationRequestId: '$context.integration.requestId',
+            status: '$context.status',
+            apiId: '$context.apiId',
+            resourcePath: '$context.resourcePath',
+            path: '$context.path',
+            resourceId: '$context.resourceId',
+            httpMethod: '$context.httpMethod',
+            sourceIp: '$context.identity.sourceIp',
+            userAgent: '$context.identity.userAgent'
+          })
+        )
+      }
+      // TODO: Add CORS Preflight
+    });
+
+    new CfnOutput(this, 'apiUrlOutput', {
+      value: API.url
+    });
+
+    // Lambda Alias
+    const alias = new Alias(this, 'LiveAlias', {
+      aliasName: 'live',
+      version: apiLambda.currentVersion,
+      provisionedConcurrentExecutions: 1
+    });
+    API.root.addProxy({
+      defaultIntegration: new LambdaIntegration(alias)
     });
   }
 }
