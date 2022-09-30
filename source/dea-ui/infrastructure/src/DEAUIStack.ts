19d9d502051b3888db6450ec648a2ee15e818590
Jennifer Clark <clrknje@amazon.com>
2022-09-30 00:07:59 -0700

    Adds UI infrastructure
    SIM: https://i.amazon.com/V716766190
    cr: https://code.amazon.com/reviews/CR-76906008

--- source/dea-ui/infrastructure/src/DEAUIStack.ts
+++ source/dea-ui/infrastructure/src/DEAUIStack.ts
@@ -4,9 +4,17 @@
  */
 
 import * as path from 'path';
-import { CfnOutput, Duration, Fn, Stack, StackProps } from 'aws-cdk-lib';
-import { AnyPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { BlockPublicAccess, Bucket, BucketAccessControl, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  AwsIntegration,
+  ContentHandling,
+  MethodOptions,
+  Model,
+  PassthroughBehavior,
+  RestApi
+} from 'aws-cdk-lib/aws-apigateway';
+import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { BlockPublicAccess, Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 import { getConstants } from './constants';
@@ -20,7 +28,6 @@ export class DEAUIStack extends Stack {
     S3_ARTIFACT_BUCKET_ARN_OUTPUT_KEY: string;
     S3_ARTIFACT_BUCKET_NAME: string;
     S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME: string;
-    ACCESS_IDENTITY_ARTIFACT_NAME: string;
   };
   // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -31,8 +38,7 @@ export class DEAUIStack extends Stack {
       AWS_REGION,
       S3_ARTIFACT_BUCKET_ARN_OUTPUT_KEY,
       S3_ARTIFACT_BUCKET_NAME,
-      S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME,
-      ACCESS_IDENTITY_ARTIFACT_NAME,
+      S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME
     } = getConstants();
     super(scope, STACK_NAME, {
       env: {
@@ -47,75 +53,84 @@ export class DEAUIStack extends Stack {
       AWS_REGION,
       S3_ARTIFACT_BUCKET_ARN_OUTPUT_KEY,
       S3_ARTIFACT_BUCKET_NAME,
-      S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME,
-      ACCESS_IDENTITY_ARTIFACT_NAME,
+      S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME
     };
-    const bucket = this._createS3Bucket(S3_ARTIFACT_BUCKET_NAME, S3_ARTIFACT_BUCKET_ARN_OUTPUT_KEY);
-    // const distribution = this._createDistribution(bucket);
-    this._deployS3Bucket(bucket);
-  }
 
-  private _addS3TLSSigV4BucketPolicy(s3Bucket: Bucket): void {
-    s3Bucket.addToResourcePolicy(
-      new PolicyStatement({
-        sid: 'Deny requests that do not use TLS/HTTPS',
-        effect: Effect.DENY,
-        principals: [new AnyPrincipal()],
-        actions: ['s3:*'],
-        resources: [s3Bucket.bucketArn, `${s3Bucket.bucketArn}/*`],
-        conditions: {
-          Bool: {
-            'aws:SecureTransport': 'false'
-          }
-        }
-      })
-    );
-    s3Bucket.addToResourcePolicy(
-      new PolicyStatement({
-        sid: 'Deny requests that do not use SigV4',
-        effect: Effect.DENY,
-        principals: [new AnyPrincipal()],
-        actions: ['s3:*'],
-        resources: [`${s3Bucket.bucketArn}/*`],
-        conditions: {
-          StringNotEquals: {
-            's3:signatureversion': 'AWS4-HMAC-SHA256'
-          }
-        }
-      })
-    );
-  }
-
-  private _createS3Bucket(bucketName: string, outputKey: string): Bucket {
-    const { S3_ACCESS_LOGS_BUCKET_PREFIX, S3_ACCESS_LOGS_BUCKET_NAME_OUTPUT_KEY } = getConstants();
-    const accessLogsBucketName: string = Fn.importValue(S3_ACCESS_LOGS_BUCKET_NAME_OUTPUT_KEY);
-    const accessLogsBucket = Bucket.fromBucketName(
-      this,
-      'imported-access-logs-bucket',
-      accessLogsBucketName
-    ) as Bucket;
-    const s3Bucket = new Bucket(this, bucketName, {
+    const bucket = new Bucket(this, S3_ARTIFACT_BUCKET_NAME, {
       accessControl: BucketAccessControl.PRIVATE,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-      serverAccessLogsBucket: accessLogsBucket,
-      serverAccessLogsPrefix: S3_ACCESS_LOGS_BUCKET_PREFIX,
-      encryption: BucketEncryption.S3_MANAGED
+      websiteIndexDocument: 'index.html'
     });
 
-    this._addS3TLSSigV4BucketPolicy(s3Bucket);
-
-    // eslint-disable-next-line no-new
-    new CfnOutput(this, outputKey, {
-      value: s3Bucket.bucketArn
-    });
-    return s3Bucket;
-  }
-
-  private _deployS3Bucket(bucket: Bucket): void {
     // eslint-disable-next-line no-new
     new BucketDeployment(this, this.distributionEnvVars.S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME, {
       destinationBucket: bucket,
-      sources: [Source.asset(path.resolve(__dirname, '../../out'))],
+      sources: [Source.asset(path.resolve(__dirname, '../../out'))]
     });
+
+    const executeRole = new Role(this, 'role', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
+    });
+
+    bucket.grantReadWrite(executeRole);
+
+    const api = new RestApi(this, 'dea-ui-gateway');
+
+    const rootS3Integration = this._getS3Integration('index.html', bucket, executeRole);
+    // GET to the root
+    api.root.addMethod('GET', rootS3Integration, this._getMethodOptions());
+
+    // GET to /{proxy+}
+    const proxy = api.root.addProxy({ anyMethod: false });
+    const proxyS3Integration = this._getS3Integration('{proxy}', bucket, executeRole);
+    proxy.addMethod('GET', proxyS3Integration, this._getMethodOptions());
+  }
+
+  private _getS3Integration(path: string, bucket: Bucket, executeRole: Role): AwsIntegration {
+    return new AwsIntegration({
+      service: 's3',
+      integrationHttpMethod: 'GET',
+      path: `${bucket.bucketName}/${path}`,
+      options: {
+        credentialsRole: executeRole,
+        requestParameters: {
+          'integration.request.path.proxy': 'method.request.path.proxy',
+          'integration.request.header.Accept': 'method.request.header.Accept',
+          'integration.request.header.Content-Type': 'method.request.header.Content-Type'
+        },
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Content-Type': 'integration.response.header.Content-Type'
+            }
+          }
+        ],
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        contentHandling: ContentHandling.CONVERT_TO_TEXT
+      }
+    });
+  }
+
+  private _getMethodOptions(): MethodOptions {
+    return {
+      requestParameters: {
+        'method.request.path.proxy': true,
+        'method.request.header.Accept': true,
+        'method.request.header.Content-Type': true
+      },
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': Model.EMPTY_MODEL
+          },
+          responseParameters: {
+            'method.response.header.Content-Length': true,
+            'method.response.header.Content-Type': true
+          }
+        }
+      ]
+    };
   }
 }
