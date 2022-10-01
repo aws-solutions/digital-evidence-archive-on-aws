09d12dde3b40535b650457093678b85c4879dc0f
Jennifer Clark <clrknje@amazon.com>
2022-09-30 17:09:00 -0700

    Updates readme and stage file
    SIM: https://i.amazon.com/V701947393
    cr: https://code.amazon.com/reviews/CR-76955584

--- source/dea-ui/infrastructure/src/constants.ts
+++ source/dea-ui/infrastructure/src/constants.ts
@@ -2,9 +2,9 @@
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
  */
-
-import * as fs from 'fs';
+import fs from 'fs';
 import { join } from 'path';
+import yaml from 'js-yaml';
 
 function getConstants(): {
   STAGE: string;
@@ -18,11 +18,15 @@ function getConstants(): {
   S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME: string;
   ACCESS_IDENTITY_ARTIFACT_NAME: string;
 } {
-  const config ={awsRegionShortName: 'sea',
-  apiUrlOutput: '',
-  awsRegion: 'us-west-2'}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const config: any = yaml.load(
+    // __dirname is a variable that reference the current directory. We use it so we can dynamically navigate to the
+    // correct file
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    fs.readFileSync(join(__dirname, `../../src/config/${process.env.STAGE}.yaml`), 'utf8') // nosemgrep
+  );
   const STAGE = process.env.STAGE || '';
-  const namePrefix = `dea-ui-${process.env.STAGE}-${config.awsRegionShortName}`;
+  const namePrefix = `dea-ui-${config.stage}-${config.awsRegionShortName}`;
   const API_BASE_URL = config.apiUrlOutput?.replace('/dev/', '') || '';
   const AWS_REGION = config.awsRegion;
   const STACK_NAME = namePrefix;
@@ -45,37 +49,8 @@ function getConstants(): {
     S3_ARTIFACT_BUCKET_ARN_OUTPUT_KEY,
     S3_ARTIFACT_BUCKET_NAME,
     S3_ARTIFACT_BUCKET_DEPLOYMENT_NAME,
-    ACCESS_IDENTITY_ARTIFACT_NAME,
+    ACCESS_IDENTITY_ARTIFACT_NAME
   };
-}
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function getAPIOutputs(): { awsRegionShortName: string; apiUrlOutput: string; awsRegion: string } {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const apiStackOutputs: any = JSON.parse(
-      // __dirname is a variable that reference the current directory. We use it so we can dynamically navigate to the
-      // correct file
-      // eslint-disable-next-line security/detect-non-literal-fs-filename
-      fs.readFileSync(join(__dirname, `../config/${process.env.STAGE}.json`), 'utf8') // nosemgrep
-    );
-    const apiStackName = Object.entries(apiStackOutputs).map(([key, value]) => key)[0]; //output has a format { stackname: {...props} }
-    // eslint-disable-next-line security/detect-object-injection
-    const outputs = apiStackOutputs[apiStackName];
-
-    if (!outputs.awsRegionShortName || !outputs.apiUrlOutput || !outputs.awsRegion) {
-      throw new Error(`Configuration file for ${process.env.STAGE} was found with incorrect format. Please deploy application swb-reference and try again.`); //validate when API unsuccessfully finished and UI is deployed
-    }
-    return {
-      awsRegionShortName: outputs.awsRegionShortName,
-      apiUrlOutput: outputs.apiUrlOutput,
-      awsRegion: outputs.awsRegion
-    };
-  } catch {
-    console.error(
-      `No API Stack deployed found for ${process.env.STAGE}.`
-    );
-    throw new Error(`No API Stack deployed found for ${process.env.STAGE}.`);
-  }
 }
 
 export { getConstants };
--- source/dea-ui/next.config.js
+++ source/dea-ui/next.config.js
@@ -1,9 +1,10 @@
-const withTM = require("next-transpile-modules")(["@awsui/components-react"]);
+const withTM = require('next-transpile-modules')(['@awsui/components-react']);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
+  basePath: '/prod'
 };
 
 module.exports = withTM(nextConfig);
