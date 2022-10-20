6510a23c886dc53b8c93425a85947570c717cd47
Jenifer <73482505+jenboydclark@users.noreply.github.com>
2022-10-20 13:03:24 -0700

    test: Updates Cloudscape testing configuration (#15)

--- source/dea-ui/jest.config.js
+++ source/dea-ui/jest.config.js
@@ -29,5 +29,12 @@ const customJestConfig = {
   preset: '@cloudscape-design/jest-preset',
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+// see https://github.com/vercel/next.js/issues/35634#issuecomment-1115250297
+async function jestConfig() {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  // /node_modules/ is the first pattern
+  nextJestConfig.transformIgnorePatterns[0] = '/node_modules/(?!@cloudscape-design)/';
+  return nextJestConfig;
+}
+
+module.exports = jestConfig;
