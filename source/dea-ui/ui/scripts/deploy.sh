53752f2efaae25448e52224343e21ee50b5c6aa5
chowchin <chowchin@amazon.com>
2022-11-11 14:58:05 -0600

    deploy update

--- source/dea-ui/ui/scripts/deploy.sh
+++ source/dea-ui/ui/scripts/deploy.sh
@@ -1,8 +1,4 @@
 ##This script can only be run by using the command `STAGE=<STAGE> rushx deploy-ui-and-api` from `/swb-ui` directory, running this script directly from this directory will not work correctly as it uses relative paths.
 apiURL='' ##Get value from swb-reference/src/config/{STAGE}.json and replace all '"' and ',' with empty.
 
-    (rushx build && NEXT_PUBLIC_API_BASE_URL=$apiURL rushx export) && 
-    (
-        cd ../infrastructure 
-        rushx cdk bootstrap && rushx cdk-deploy
-    )
+rushx build && NEXT_PUBLIC_API_BASE_URL=$apiURL rushx export
