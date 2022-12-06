9d1a5a38f957908b0b2232dea409a73c1c252f87
Danielle Melvin <melvdani@amazon.com>
2022-12-05 22:15:32 -0500

    feature: add hello world api endpoint with backing lambda
    This will be used for testing the Custom Lambda Authorizer. Once
    other APIs are available for testing, this endpoint will be removed
    and/or only used for testing

---
+++ source/dea-backend/src/lambda-handlers/hello-world-handler.ts
@@ -0,0 +1,20 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
+
+exports.handler = async function (event: APIGatewayEvent, context: Context,
+                   callback: APIGatewayProxyCallback) {
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          status: "success",
+          message: 'Hello World!'
+        }),
+      };
+};
