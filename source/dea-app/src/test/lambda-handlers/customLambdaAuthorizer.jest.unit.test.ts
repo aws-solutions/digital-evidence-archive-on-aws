d708cac84af6487894206dff88506f0eb72cc2fc
Danielle Melvin <melvdani@amazon.com>
2022-12-07 14:30:56 -0500

    feat: add skeleton of a lambda authorizer with basic unit tests
    Lambda Authorizer with Basic Authorization. When the Cognito User Pool
    is added, it will be used for authentication in the Authorizer, and the
    unit tests will be updated

---
+++ source/dea-app/src/test/lambda-handlers/customLambdaAuthorizer.jest.unit.test.ts
@@ -0,0 +1,159 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  APIGatewayAuthorizerCallback,
+  APIGatewayAuthorizerResult,
+  APIGatewayTokenAuthorizerEvent,
+  PolicyDocument,
+} from 'aws-lambda';
+import { handler } from '../../lambda-handlers/custom-lambda-authorizer';
+
+type LambdaResult = APIGatewayAuthorizerResult | Error | string | undefined;
+
+const METHOD_ARN = 'arn:aws:execute-api:us-east-1:account-id:api-id/*/GET/my-cases';
+// TODO: When Cognito is added, mock it to test the logic of the interactions,
+const ALLOW_TOKEN = 'allow';
+const DENY_TOKEN = 'deny';
+const UNAUTHORIZED_TOKEN = 'unauthorized';
+const INVALID_TOKEN = 'aaaabbbbccc';
+
+const ALLOW_POLICY_DOCUMENT: PolicyDocument = {
+  Version: '2012-10-17',
+  Statement: [
+    {
+      Action: 'execute-api:Invoke',
+      Effect: 'Allow',
+      Resource: METHOD_ARN,
+    },
+  ],
+};
+const ALLOW_POLICY_DOCUMENT_STRING = JSON.stringify(ALLOW_POLICY_DOCUMENT);
+
+const DENY_POLICY_DOCUMENT: PolicyDocument = {
+  Version: '2012-10-17',
+  Statement: [
+    {
+      Action: 'execute-api:Invoke',
+      Effect: 'Deny',
+      Resource: METHOD_ARN,
+    },
+  ],
+};
+const DENY_POLICY_DOCUMENT_STRING = JSON.stringify(DENY_POLICY_DOCUMENT);
+
+//this is covered already and provided as an example of jest import mocking
+describe('Unit tests for custom lambda authorizer', () => {
+  it('verifies successful response', async () => {
+    const event: APIGatewayTokenAuthorizerEvent = createEvent(ALLOW_TOKEN);
+
+    let lambdaResult: LambdaResult;
+    const callback: APIGatewayAuthorizerCallback = (
+      error?: Error | string | null,
+      result?: APIGatewayAuthorizerResult
+    ) => {
+      if (result) {
+        lambdaResult = result;
+      } else {
+        lambdaResult = error === null ? undefined : error;
+      }
+    };
+
+    await handler(event, callback);
+
+    expect(isAnAuthResult(lambdaResult)).toBe(true);
+
+    // eslint-disable-next-line
+    const authResult = lambdaResult as APIGatewayAuthorizerResult;
+    expect(authResult.principalId).toEqual('user');
+    expect(JSON.stringify(authResult.policyDocument)).toEqual(ALLOW_POLICY_DOCUMENT_STRING);
+  });
+
+  it('verifies deny response', async () => {
+    const event: APIGatewayTokenAuthorizerEvent = createEvent(DENY_TOKEN);
+
+    let lambdaResult: LambdaResult;
+    const callback: APIGatewayAuthorizerCallback = (
+      error?: Error | string | null,
+      result?: APIGatewayAuthorizerResult
+    ) => {
+      if (result) {
+        lambdaResult = result;
+      } else {
+        lambdaResult = error === null ? undefined : error;
+      }
+    };
+
+    await handler(event, callback);
+
+    expect(isAnAuthResult(lambdaResult)).toBe(true);
+
+    // eslint-disable-next-line
+    const authResult = lambdaResult as APIGatewayAuthorizerResult;
+    expect(authResult.principalId).toEqual('user');
+    expect(JSON.stringify(authResult.policyDocument)).toEqual(DENY_POLICY_DOCUMENT_STRING);
+  });
+
+  it('verifies unauthorized response', async () => {
+    const event: APIGatewayTokenAuthorizerEvent = createEvent(UNAUTHORIZED_TOKEN);
+
+    let lambdaResult: LambdaResult;
+    const callback: APIGatewayAuthorizerCallback = (
+      error?: Error | string | null,
+      result?: APIGatewayAuthorizerResult
+    ) => {
+      if (result) {
+        lambdaResult = result;
+      } else {
+        lambdaResult = error === null ? undefined : error;
+      }
+    };
+
+    await handler(event, callback);
+
+    expect(typeof lambdaResult).toBe('string');
+
+    // eslint-disable-next-line
+    const authResult = lambdaResult as string;
+    expect(lambdaResult).toEqual('Unauthorized');
+  });
+
+  it('verifies invalid token response', async () => {
+    const event: APIGatewayTokenAuthorizerEvent = createEvent(INVALID_TOKEN);
+
+    let lambdaResult: LambdaResult;
+    const callback: APIGatewayAuthorizerCallback = (
+      error?: Error | string | null,
+      result?: APIGatewayAuthorizerResult
+    ) => {
+      if (result) {
+        lambdaResult = result;
+      } else {
+        lambdaResult = error === null ? undefined : error;
+      }
+    };
+
+    await handler(event, callback);
+
+    expect(typeof lambdaResult).toBe('string');
+
+    // eslint-disable-next-line
+    const authResult = lambdaResult as string;
+    expect(lambdaResult).toEqual('Error: Invalid token');
+  });
+});
+
+function createEvent(token: string): APIGatewayTokenAuthorizerEvent {
+  return {
+    type: 'TOKEN',
+    methodArn: METHOD_ARN,
+    authorizationToken: token,
+  };
+}
+
+// eslint-disable-next-line
+function isAnAuthResult(lambdaResult: any): boolean {
+  return 'principalId' in lambdaResult && 'policyDocument' in lambdaResult;
+}
