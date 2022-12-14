27657e25613920263425c5d9a6cdaf2248060949
Dan <dwood0@gmail.com>
2022-12-14 15:17:53 -0700

    fix: use explicit table name for reference by onetable (#64)

--- source/dea-backend/src/constructs/dea-backend-stack.ts
+++ source/dea-backend/src/constructs/dea-backend-stack.ts
@@ -4,19 +4,9 @@
  */
 
 /* eslint-disable no-new */
-import { RemovalPolicy, StackProps } from 'aws-cdk-lib';
+import { CfnResource, RemovalPolicy, StackProps } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, ProjectionType, Table, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
-import {
-  CfnSecurityGroup,
-  FlowLog,
-  FlowLogDestination,
-  FlowLogResourceType,
-  SubnetType,
-  Vpc,
-} from 'aws-cdk-lib/aws-ec2';
-import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Key } from 'aws-cdk-lib/aws-kms';
-import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 import { getConstants } from '../constants';
 
@@ -26,15 +16,11 @@ interface IBackendStackProps extends StackProps {
 
 export class DeaBackendConstruct extends Construct {
   public deaTable: Table;
-  public deaVpc: Vpc;
 
   public constructor(scope: Construct, id: string, props: IBackendStackProps) {
     const { STACK_NAME } = getConstants();
 
     super(scope, STACK_NAME);
-
-    //take a optional VPC from config, if not provided create one
-    this.deaVpc = this._createVpc(props.kmsKey);
 
     //Dynamo
     this.deaTable = this._createDeaTable(props.kmsKey);
@@ -42,9 +28,10 @@ export class DeaBackendConstruct extends Construct {
 
   private _createDeaTable(key: Key): Table {
     const deaTable = new Table(this, 'DeaTable', {
+      tableName: 'DeaTable',
       billingMode: BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: 'PK', type: AttributeType.STRING },
-      // removalPolicy: RemovalPolicy.RETAIN,
+      //should probably be RETAIN later
       removalPolicy: RemovalPolicy.DESTROY,
       sortKey: { name: 'SK', type: AttributeType.STRING },
       encryption: TableEncryption.CUSTOMER_MANAGED,
@@ -66,50 +53,19 @@ export class DeaBackendConstruct extends Construct {
       sortKey: { name: 'GSI2SK', type: AttributeType.STRING },
     });
 
+    const tableNode = deaTable.node.defaultChild;
+    if (tableNode instanceof CfnResource) {
+      tableNode.addMetadata('cfn_nag', {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        rules_to_suppress: [
+          {
+            id: 'W28',
+            reason: 'Table requires an explicit name to be referenced by Onetable',
+          }
+        ],
+      });
+    }
+
     return deaTable;
-  }
-
-  private _createVpc(key: Key): Vpc {
-    const vpc = new Vpc(this, 'dea-vpc', {
-      natGateways: 0,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'Ingress',
-          subnetType: SubnetType.PRIVATE_ISOLATED,
-        },
-      ],
-    });
-
-    new CfnSecurityGroup(this, 'vpc-sg', {
-      groupDescription: 'dea-vpc security group',
-      securityGroupEgress: [
-        {
-          ipProtocol: 'tcp',
-
-          // the properties below are optional
-          cidrIp: '0.0.0.0/32',
-          fromPort: 1,
-          toPort: 1,
-          description: 'egress rule for dea vpc',
-        },
-      ],
-      vpcId: vpc.vpcId,
-    });
-
-    const logGroup = new LogGroup(this, 'dea-vpc-log-group', {
-      encryptionKey: key,
-    });
-
-    const role = new Role(this, 'flow-log-role', {
-      assumedBy: new ServicePrincipal('vpc-flow-logs.amazonaws.com'),
-    });
-
-    new FlowLog(this, 'FlowLog', {
-      resourceType: FlowLogResourceType.fromVpc(vpc),
-      destination: FlowLogDestination.toCloudWatchLogs(logGroup, role),
-    });
-
-    return vpc;
   }
 }
