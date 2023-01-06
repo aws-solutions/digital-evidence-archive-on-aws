63d3909e68b6545122521b2175e5009cbe56a713
Daniel Wood <dwood0@gmail.com>
2023-01-05 18:34:06 -0700

    feat: add endpoint to fetch details for a case

--- source/dea-backend/src/test/infra/dea-backend-constructs.unit.test.ts
+++ source/dea-backend/src/test/infra/dea-backend-constructs.unit.test.ts
@@ -50,8 +50,8 @@ describe('DeaBackend constructs', () => {
     });
 
     //handlers + authorizer
-    template.resourceCountIs('AWS::Lambda::Function', 8);
-    template.resourceCountIs('AWS::ApiGateway::Method', 7);
+    template.resourceCountIs('AWS::Lambda::Function', 9);
+    template.resourceCountIs('AWS::ApiGateway::Method', 8);
 
     expect.addSnapshotSerializer({
       test: (val) => typeof val === 'string' && val.includes('zip'),
