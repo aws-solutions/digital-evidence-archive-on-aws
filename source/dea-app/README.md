f994f4abd5ad8ff23904ce492e3adac2b2504e37
Daniel Wood <dwood0@gmail.com>
2023-01-25 08:59:05 -0700

    test: add integration tests for delete-cases and get-all-cases resources

--- source/dea-app/README.md
+++ source/dea-app/README.md
@@ -6,7 +6,7 @@
 
 | Statements                                                                         | Branches                                                                      | Functions                                                                        | Lines                                                                   |
 | ---------------------------------------------------------------------------------- | ----------------------------------------------------------------------------- | -------------------------------------------------------------------------------- | ----------------------------------------------------------------------- |
-| ![Statements](https://img.shields.io/badge/statements-73.26%25-red.svg?style=flat) | ![Branches](https://img.shields.io/badge/branches-32.22%25-red.svg?style=flat) | ![Functions](https://img.shields.io/badge/functions-67.16%25-red.svg?style=flat) | ![Lines](https://img.shields.io/badge/lines-71.66%25-red.svg?style=flat) |
+| ![Statements](https://img.shields.io/badge/statements-81.18%25-yellow.svg?style=flat) | ![Branches](https://img.shields.io/badge/branches-36.66%25-red.svg?style=flat) | ![Functions](https://img.shields.io/badge/functions-73.13%25-red.svg?style=flat) | ![Lines](https://img.shields.io/badge/lines-80%25-yellow.svg?style=flat) |
 
 
 This project is the routing package for `dea-backend`. It is used by `dea-backend` to generate routes for DEA API. Please refer [here](../dea-backend/README.md) for more information on how to use this project.
---
+++ source/dea-app/src/test/app/resources/delete-cases.integration.test.ts
@@ -0,0 +1,71 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fail } from "assert";
+import { ValidationError } from "../../../app/exceptions/validation-exception";
+import { deleteCase } from "../../../app/resources/delete-cases";
+import { getCase } from "../../../app/services/case-service";
+import { DeaCase } from "../../../models/case";
+import { CaseStatus } from "../../../models/case-status";
+import { createCase } from "../../../persistence/case";
+import { ModelRepositoryProvider } from "../../../persistence/schema/entities";
+import { dummyContext, dummyEvent } from "../../integration-objects";
+import { getTestRepositoryProvider } from "./get-test-repository";
+
+let repositoryProvider: ModelRepositoryProvider;
+
+describe('delete cases resource', () => {
+
+    beforeAll(async () => {
+        repositoryProvider = await getTestRepositoryProvider('deleteCasesTest');
+    });
+
+    afterAll(async () => {
+        await repositoryProvider.table.deleteTable('DeleteTableForever');
+    });
+
+    it('should respond with success if the target entity does not exist', async () => {
+        const event = Object.assign({}, {
+            ...dummyEvent,
+            pathParameters: {
+                caseId: '01ARZ3NDEKTSV4RRFFQ69G5FAV',
+            },
+        });
+
+        const response = await deleteCase(event, dummyContext, repositoryProvider);
+
+        expect(response.statusCode).toEqual(204);
+    });
+
+    it('should successfully delete a case', async () => {
+        const theCase: DeaCase = {
+            name: "ACaseForDeleting",
+            status: CaseStatus.ACTIVE,
+            description: "An initial description",
+        };
+        const createdCase = await createCase(theCase, repositoryProvider);
+        if (!createdCase.ulid) {
+            fail();
+        }
+
+        const event = Object.assign({}, {
+            ...dummyEvent,
+            pathParameters: {
+                caseId: createdCase.ulid,
+            },
+        });
+
+        const response = await deleteCase(event, dummyContext, repositoryProvider);
+
+        expect(response.statusCode).toEqual(204);
+
+        const deletedCase = await getCase(createdCase.ulid, repositoryProvider);
+        expect(deletedCase).toBeUndefined();
+    });
+
+    it('should error if the path param is not provided', async () => {
+        await expect(deleteCase(dummyEvent, dummyContext, repositoryProvider)).rejects.toThrow(ValidationError);
+    });
+});
