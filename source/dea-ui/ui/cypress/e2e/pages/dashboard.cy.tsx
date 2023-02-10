14f2cf30a7b3f07afec466fc69507de0f1971742
Daniel Wood <dwood0@gmail.com>
2023-02-10 11:52:24 -0700

    tests: configure cypress to run against deployed ui

--- source/dea-ui/ui/cypress/e2e/pages/dashboard.cy.tsx
+++ source/dea-ui/ui/cypress/e2e/pages/dashboard.cy.tsx
@@ -1,12 +1,11 @@
 describe('DEA Dashboard', () => {
   it('should have a header', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/test/ui')
-    
+    cy.visit('/');
 
     // The new page should contain a header
     cy.get('Header').should('have.text', 'Digital Evidence Archive on AWSSherlock Holmes');
-  })
-})
+  });
+});
 
-export {}
+export {};
