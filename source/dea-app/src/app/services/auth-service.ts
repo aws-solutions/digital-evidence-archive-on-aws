72219ee2829cdd15dfc15c6a17ab38c1c2143bbf
chowchin <chowchin@amazon.com>
2023-02-10 15:26:41 -0600

    update ssm param get

--- source/dea-app/src/app/services/auth-service.ts
+++ source/dea-app/src/app/services/auth-service.ts
@@ -10,20 +10,6 @@ import { getRequiredEnv } from '../../lambda-http-helpers';
 const stage = getRequiredEnv('STAGE', 'chewbacca');
 const region = getRequiredEnv('AWS_REGION', 'us-east-1');
 
-export async function getSSMParam(ssmClient: SSMClient, paramPath: string) {
-  const cmd = new GetParametersCommand({
-    Names: [paramPath],
-  });
-
-  const result = await ssmClient.send(cmd);
-
-  if (result.Parameters && result.Parameters[0]) {
-    return result.Parameters[0].Value;
-  } else {
-    throw new Error(`Parameter not found: ${paramPath}`);
-  }
-}
-
 export const getCognitoSsmParams = async (): Promise<[string, string, string]> => {
   const ssmClient = new SSMClient({ region });
 
@@ -31,14 +17,40 @@ export const getCognitoSsmParams = async (): Promise<[string, string, string]> =
   const clientIdPath = `/dea/${region}/${stage}-userpool-client-id-param`;
   const callbackUrlPath = `/dea/${region}/${stage}-client-callback-url-param`;
 
-  const cognitoDomainUrl = await getSSMParam(ssmClient, cognitoDomainPath);
-  const clientId = await getSSMParam(ssmClient, clientIdPath);
-  const callbackUrl = await getSSMParam(ssmClient, callbackUrlPath);
+  const response = await ssmClient.send(
+    new GetParametersCommand({
+      Names: [cognitoDomainPath, clientIdPath, callbackUrlPath],
+    })
+  );
+
+  if (!response.Parameters) {
+    throw new Error(`No parameters found for: ${cognitoDomainPath}, ${clientIdPath}, ${callbackUrlPath}`);
+  }
+
+  let cognitoDomainUrl;
+  let clientId;
+  let callbackUrl;
+
+  response.Parameters.forEach((param) => {
+    switch (param.Name) {
+      case cognitoDomainPath:
+        cognitoDomainUrl = param.Value;
+        break;
+      case clientIdPath:
+        clientId = param.Value;
+        break;
+      case callbackUrlPath:
+        callbackUrl = param.Value;
+        break;
+    }
+  });
 
   if (cognitoDomainUrl && clientId && callbackUrl) {
     return [cognitoDomainUrl, clientId, callbackUrl];
   } else {
-    throw new Error('Unable to grab the parameters in SSM needed for token verification.');
+    throw new Error(
+      `Unable to grab the parameters in SSM needed for token verification: ${cognitoDomainUrl}, ${clientId}, ${callbackUrl}`
+    );
   }
 };
 
