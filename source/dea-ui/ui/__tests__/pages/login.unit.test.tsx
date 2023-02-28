1c229e6baae31dabfe6cb3c3515f3504c8c96aa9
chowchin <chowchin@amazon.com>
2023-02-28 00:50:43 -0600

    unit test fix

--- source/dea-ui/ui/__tests__/pages/login.unit.test.tsx
+++ source/dea-ui/ui/__tests__/pages/login.unit.test.tsx
@@ -3,6 +3,13 @@ import { render, screen } from '@testing-library/react';
 import { commonLabels } from '../../src/common/labels';
 import LoginPage from '../../src/pages/login';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    query: { code: 'abcdefg' },
+    push: jest.fn(),
+  })),
+}));
+
 describe('LoginPage', () => {
   it('renders a login page when waiting to log in', async () => {
     const page = render(<LoginPage />);
