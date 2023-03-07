7beeaa539acff9ff22e2de64405431d72a2c23fc
chowchin <chowchin@amazon.com>
2023-03-07 09:50:19 -0600

    copyright

--- source/dea-ui/ui/src/context/AuthenticationContext.tsx
+++ source/dea-ui/ui/src/context/AuthenticationContext.tsx
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import jwt from 'jwt-decode';
 import { useRouter } from 'next/router';
 import { createContext, useContext, Context, useState, useEffect } from 'react';
