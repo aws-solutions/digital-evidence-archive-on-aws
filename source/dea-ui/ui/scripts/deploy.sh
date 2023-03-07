89327d7683fd45d22579c05eb1f6653919b682cf
Daniel Wood <dwood0@gmail.com>
2023-03-07 10:04:24 -0700

    documentation: readme updates

--- source/dea-ui/ui/scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-##This script can only be run by using the command `STAGE=<STAGE> rushx deploy-ui-and-api` from `/swb-ui` directory, running this script directly from this directory will not work correctly as it uses relative paths.
-apiURL='' ##Get value from swb-reference/src/config/{STAGE}.json and replace all '"' and ',' with empty.
-
-NEXT_PUBLIC_API_BASE_URL=$apiURL rushx export
