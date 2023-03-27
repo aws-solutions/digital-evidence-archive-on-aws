2f2f0436e9ebadbe694ebf8dbe5995f4c000828b
Dan <dwood0@gmail.com>
2023-03-27 11:42:21 -0600

    fix: add defaults to some scripts (#194)

--- source/dea-ui/ui/setEnv.sh
+++ source/dea-ui/ui/setEnv.sh
@@ -1,3 +1,3 @@
 # https://nextjs.org/docs/basic-features/environment-variables
 echo NEXT_PUBLIC_DEA_API_URL=$DEA_API_URL > .env.local
-echo NEXT_PUBLIC_STAGE=$STAGE >> .env.local
+echo NEXT_PUBLIC_STAGE=${STAGE:-chewbacca} >> .env.local
