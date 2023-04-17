bd06a0396056ec41203d94277fcc30026834e82c
Danielle Melvin <melvdani@amazon.com>
2023-04-17 19:20:27 -0400

    add unit test for retries

--- source/dea-app/src/app/services/service-helpers.ts
+++ source/dea-app/src/app/services/service-helpers.ts
@@ -3,14 +3,14 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-export async function retry<T>(fn: () => Promise<T>, retries = 10, interval = 500) {
+export async function retry<T>(fn: () => Promise<T>, retries = 5, intervalInMs = 300) {
   for (let i = 0; i < retries; i++) {
     try {
       return await fn();
     } catch (e) {
       if (i < retries - 1) {
         // Random backoff
-        const delayInMs = interval + randInt(0, 50);
+        const delayInMs = intervalInMs;
         await sleep(delayInMs);
       } else {
         throw e;
@@ -20,5 +20,3 @@ export async function retry<T>(fn: () => Promise<T>, retries = 10, interval = 50
 }
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const randInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
