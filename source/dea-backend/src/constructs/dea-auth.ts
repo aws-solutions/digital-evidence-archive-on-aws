0a071cb8f588abb3db08d4d0bd29c359dea63495
chowchin <chowchin@amazon.com>
2023-06-02 12:51:09 -0500

    chore: add regex check for domain name

--- source/dea-backend/src/constructs/dea-auth.ts
+++ source/dea-backend/src/constructs/dea-auth.ts
@@ -406,6 +406,7 @@ export class DeaAuth extends Construct {
       const cognitoPrefixParam = new CfnParameter(this, 'CognitoDomainPrefix', {
         type: 'String',
         description: 'The prefix of the cognito domain to associate to the user pool',
+        allowedPattern: '^[a-z][a-z0-9-]*$',
       });
       domainPrefix = cognitoPrefixParam.valueAsString;
     }
