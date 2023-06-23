47184e0fe72bc13807d4da1d98838e455d915741
drew-pecka <drew_pecka@hotmail.com>
2023-06-22 21:37:13 -0400

    updating links to GItHub

--- source/release.config.js
+++ source/release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "ssh://git.amazon.com/pkg/Digital-evidence-archive",
+  branches: 'main',
+  repositoryUrl: 'git@github.com:aws-solutions/digital-evidence-archive-on-aws.git',
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
   ],
 };
