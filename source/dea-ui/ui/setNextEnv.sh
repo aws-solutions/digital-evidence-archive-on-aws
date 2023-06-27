785af15693313349e20fee9256ba70d5c0a6ad5b
Denis Quesada <denisqu@amazon.com>
2023-06-27 18:42:10 +0000

    fix: handle region for custom domain deployment.

--- source/dea-ui/ui/setNextEnv.sh
+++ source/dea-ui/ui/setNextEnv.sh
@@ -2,3 +2,4 @@
 echo NEXT_PUBLIC_DEA_API_URL=$DEA_API_URL > .env.local
 echo NEXT_PUBLIC_STAGE=${STAGE:-chewbacca} >> .env.local
 echo NEXT_PUBLIC_IS_USING_CUSTOM_DOMAIN=${DEA_CUSTOM_DOMAIN} >> .env.local
+echo NEXT_PUBLIC_AWS_REGION=${AWS_REGION} >> .env.local
