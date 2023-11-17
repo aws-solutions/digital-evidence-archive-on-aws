57020a3ad50edc8a5909fdb8ba32f062fffd7aaf
Daniel Wood <ddubs@amazon.com>
2023-11-17 16:03:49 -0700

    fix: be more verbose with joi validation error messages
    cr: https://code.amazon.com/reviews/CR-106409562

--- source/dea-app/src/models/validation/joi-common.ts
+++ source/dea-app/src/models/validation/joi-common.ts
@@ -10,26 +10,36 @@ import { CaseStatus } from '../case-status';
 export const ONE_MB = 1024 * 1024;
 export const ONE_TB = 1024 * 1024 * 1024 * 1024;
 
-// Override the default messages to get rid of the inputted value from the message.
-const genericPatternMessage = '{{#label}} fails to match the pattern';
-const customMessages = {
-  'string.pattern.base': genericPatternMessage,
-  'string.pattern.name': genericPatternMessage,
-  'string.pattern.invert.base': genericPatternMessage,
-  'string.pattern.invert.name': genericPatternMessage,
-};
+function customJoiMessages(customMessage: string) {
+  return {
+    'string.pattern.base': customMessage,
+    'string.pattern.name': customMessage,
+    'string.pattern.invert.base': customMessage,
+    'string.pattern.invert.name': customMessage,
+  };
+}
 
 export const allButDisallowed = new RegExp('^[^\\<>/]+$');
+const allButDisallowedMessage = 'The following characters are not allowed in {{#label}}: \\ < > /';
 
 export const filenameSafeCharsRegex = new RegExp('^[^/\\0]+$');
+const filenameSafeCharsMessage = 'Forward slash and null characters are not allowed in {{#label}}';
 
 // using unix convention. will have to see what we need to do to be unix and windows compatible
 // allow '/' for root directory, enforce path starts and ends with '/' for anything else
 export const filePathSafeCharsRegex = new RegExp('^(/[^<>:"\\|?*]+/|/)$');
+const filePathSafeCharsMessage = 'The following characters are not allowed in {{#label}}: < > : " ? *';
 
 export const htmlSafeCharsRegex = new RegExp('^[^&"\'<>]*$');
+const htmlSafeMessage = 'The following characters are not allowed in {{#label}}: & " \' < >';
 
 export const ulidRegex = new RegExp('^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$');
+const ulidMessage = '{{#label}} is not a valid ULID';
+
+const sha256HashRegex = new RegExp('^[a-fA-F0-9]{64}$');
+const sha256HashMessage = '{{#label}} is not a valid SHA256 hash value';
+
+const arnMessage = '{{#label}} is not a valid ARN';
 
 export const taskIdRegex = new RegExp('^task-[0-9a-z]{17}$');
 
@@ -39,32 +49,48 @@ export const taskReportRegex = new RegExp(
 
 export const jtiRegex = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$');
 
-export const s3Identifier = Joi.string().pattern(htmlSafeCharsRegex).messages(customMessages);
+export const s3Identifier = Joi.string()
+  .pattern(htmlSafeCharsRegex)
+  .messages(customJoiMessages(htmlSafeMessage));
 
-export const joiUlid = Joi.string().pattern(ulidRegex).messages(customMessages);
+export const joiUlid = Joi.string().pattern(ulidRegex).messages(customJoiMessages(ulidMessage));
 
 export const joiUuid = Joi.string().uuid();
 
-export const sha256Hash = Joi.string().pattern(new RegExp('^[a-fA-F0-9]{64}$')).messages(customMessages);
+export const sha256Hash = Joi.string()
+  .pattern(sha256HashRegex)
+  .messages(customJoiMessages(sha256HashMessage));
 
-export const joiArn = Joi.string().pattern(new RegExp('.*\\S.*')).min(20).max(2048).messages(customMessages);
+export const joiArn = Joi.string()
+  .pattern(new RegExp('.*\\S.*'))
+  .min(20)
+  .max(2048)
+  .messages(customJoiMessages(arnMessage));
 
 export const safeName = Joi.string()
   .pattern(allButDisallowed)
   .required()
   .min(3)
   .max(50)
-  .messages(customMessages);
+  .messages(customJoiMessages(allButDisallowedMessage));
 
-export const safeReason = Joi.string().pattern(allButDisallowed).min(2).max(250).messages(customMessages);
-export const safeDetails = Joi.string().pattern(allButDisallowed).min(2).max(250).messages(customMessages);
+export const safeReason = Joi.string()
+  .pattern(allButDisallowed)
+  .min(2)
+  .max(250)
+  .messages(customJoiMessages(allButDisallowedMessage));
+export const safeDetails = Joi.string()
+  .pattern(allButDisallowed)
+  .min(2)
+  .max(250)
+  .messages(customJoiMessages(allButDisallowedMessage));
 
 export const fileName = Joi.string()
   .pattern(filenameSafeCharsRegex)
   .required()
   .max(255)
   .required()
-  .messages(customMessages);
+  .messages(customJoiMessages(filenameSafeCharsMessage));
 
 export const filePath = Joi.string()
   .pattern(filePathSafeCharsRegex)
@@ -72,19 +98,22 @@ export const filePath = Joi.string()
   .min(1)
   .max(500)
   .required()
-  .messages(customMessages);
+  .messages(customJoiMessages(filePathSafeCharsMessage));
 
-export const contentType = Joi.string().pattern(htmlSafeCharsRegex).max(200).messages(customMessages);
+export const contentType = Joi.string()
+  .pattern(htmlSafeCharsRegex)
+  .max(200)
+  .messages(customJoiMessages(htmlSafeMessage));
 
 export const caseStatus = Joi.string().valid(...Object.keys(CaseStatus));
 
 export const idToken = Joi.string()
   .regex(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/)
-  .messages(customMessages);
+  .messages(customJoiMessages('Invalid ID Token'));
 
 export const refreshToken = Joi.string()
   .regex(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/)
-  .messages(customMessages);
+  .messages(customJoiMessages('Invalid refresh token'));
 
 export const loginUrlRegex = Joi.string().uri();
 
@@ -95,7 +124,7 @@ export const safeDescription = Joi.string()
   .max(200)
   .min(3)
   .optional()
-  .messages(customMessages);
+  .messages(customJoiMessages(allButDisallowedMessage));
 
 export const paginationLimit = Joi.number().min(1).max(10000).optional();
 
@@ -103,7 +132,7 @@ export const base64String = Joi.string().base64().required();
 
 export const ttlJoi = Joi.number().min(1500000000).max(5000000000).required();
 
-export const jti = Joi.string().pattern(jtiRegex).required().messages(customMessages);
+export const jti = Joi.string().pattern(jtiRegex).required().messages(customJoiMessages('Invalid JWT Id'));
 
 export const taskIdJoi = Joi.string().pattern(taskIdRegex).required();
 
@@ -112,7 +141,7 @@ export const taskReportJoi = Joi.string().pattern(taskReportRegex).required();
 // https://github.com/odomojuli/RegExAPI
 export const authCode = Joi.string()
   .regex(/^[A-Za-z0-9-_]+$/)
-  .messages(customMessages);
+  .messages(customJoiMessages('Invalid Auth code'));
 
 // FileSize should be a positive integer less than 5 TB
 export const safeFileSize = Joi.number()
